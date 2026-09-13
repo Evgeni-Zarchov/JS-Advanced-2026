9b76750cd72979627c628603df116e72677d88eb
Evgeni-Zarchov <ezarchov@gmail.com>
2026-09-13 10:25:00 +0300

    Delete 01-SyntaxFunctionsAndStatements-Lab directory

--- 01-SyntaxFunctionsAndStatements-Lab/01-EchoFunction.js
+++
@@ -1,9 +0,0 @@
-function echo(str) {
-    let length = str.length;
-
-    console.log(length);
-    console.log(str);
-}
-
-echo("Hello, JavaScript!");
-echo("strings are easy");
--- 01-SyntaxFunctionsAndStatements-Lab/08-DaysInAMonth.js
+++
@@ -1,5 +0,0 @@
-function daysInMonth(month, year) {
-    return date = new Date(year, month, 0).getDate();
-}
-
-console.log(daysInMonth(1, 2012));
